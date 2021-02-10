3f5dc37e260be06ac7993e05dbe55ed854ed993b
Jellian <haanniiaarodriguez@gmail.com>
2021-02-09 19:55:44 -0600

    cambios de la tarde

--- app/src/main/java/com/fiuady/volley/MainActivity.kt
+++ app/src/main/java/com/fiuady/volley/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.*
-import com.mashape.unirest.http.Unirest
+import org.json.JSONArray
 import org.json.JSONObject
 
 
@@ -17,9 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var Delete:Button
     private lateinit var Read:Button
     private lateinit var Username:EditText
-    private lateinit var Changename:EditText
-    private lateinit var clave:TextView
-    private lateinit var  response: HttpResponse
+    private lateinit var create:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,63 +28,51 @@ class MainActivity : AppCompatActivity() {
         Update=findViewById(R.id.update)
         Delete=findViewById(R.id.delete)
         Read=findViewById(R.id.read)
-        Username=findViewById(R.id.username)
-        Changename=findViewById(R.id.user)
-        clave=findViewById(R.id.clave)
+        Username=findViewById(R.id.iduser)
+        create=findViewById(R.id.usernew)
 
         val jsonobj=JSONObject()
         val url="https://quizapp3-55aa.restdb.io/rest/usersgame?apikey=93197c60759dca33a6dd2862665a0f9a900b9"
         val queue=Volley.newRequestQueue(this)
 
+
         Create.setOnClickListener {
-            jsonobj.put("name", Changename.text)
-            val req=JsonObjectRequest(
-                Request.Method.POST,
-                url,
-                jsonobj,
-                Response.Listener { },
-                Response.ErrorListener { })
+            jsonobj.put("name",Username.text)
+            val req=JsonObjectRequest(Request.Method.POST,url,jsonobj,Response.Listener {  },Response.ErrorListener {  })
             queue.add(req)
         }
 
         Update.setOnClickListener {
-            jsonobj.put("id", Username.text)
-            val req=JsonObjectRequest(
-                Request.Method.PUT,
-                url,
-                jsonobj,
-                Response.Listener { },
-                Response.ErrorListener { })
+            jsonobj.put("id", create.text)
+            val req=JsonObjectRequest(Request.Method.PUT,url,jsonobj,Response.Listener {  },Response.ErrorListener {  })
             queue.add(req)
         }
 
         Delete.setOnClickListener {
-            val id2=clave.text
-            val url2="https://quizapp3-55aa.restdb.io/rest/usersgame/$id2?apikey=93197c60759dca33a6dd2862665a0f9a900b9"
-            val req=JsonObjectRequest(Request.Method.DELETE, url2, jsonobj, Response.Listener {}, Response.ErrorListener { })
+            val id=Username.text
+            val urldelete="https://quizapp3-55aa.restdb.io/rest/usersgame/$id?apikey=93197c60759dca33a6dd2862665a0f9a900b9"
+            val req=JsonObjectRequest(Request.Method.DELETE,urldelete,jsonobj,Response.Listener {  },Response.ErrorListener {  })
             queue.add(req)
         }
 
         Read.setOnClickListener {
+            val id=Username.text
+            val urlget="https://quizapp3-55aa.restdb.io/rest/usersgame/$id?apikey=93197c60759dca33a6dd2862665a0f9a900b9"
+            val StringRequest= StringRequest(Request.Method.GET, urlget, Response.Listener { response ->
+                val jsonObject=JSONObject(response)
+                Text.text = jsonObject.get("name").toString()
 
-           val id=Username.text
-           /* val response= Unirest.get("https://quizapp3-55aa.restdb.io/rest/usersgame?q={"id":$id}")
-                .header("x-apikey", "93197c60759dca33a6dd2862665a0f9a900b9")
-                .header("cache-control", "no-cache")
-                .asString()
-            var all =response.body
-            var allres=JSONObject(all).getString("name")
-            Text.text=allres.toString()*/
-            
-        val urlget="https://quizapp3-55aa.restdb.io/rest/usersgame?q={\"id\":$id}?apikey=93197c60759dca33a6dd2862665a0f9a900b9"
-            val StringRequest= StringRequest(
-                Request.Method.GET,
-                urlget,
-                Response.Listener<String> { response ->
-                    val obj = JSONObject(response)
-                    Text.text = obj.getString("name")
-                },
-                Response.ErrorListener {})
+                /*var y=0
+                val jsonArray= JSONArray(response)
+                val i= Username.text.toString().toInt()
+                val jsonObject=JSONObject(jsonArray.getString(i))
+                while(y <  jsonArray.length() ){
+                    if(i.toString()==JSONObject(jsonObject.toString(y)).getString("id")){
+                        Text.text=JSONObject(jsonObject.toString(y)).getString("id")
+                    }
+                    y++
+                }*/
+            }, Response.ErrorListener {})
             queue.add(StringRequest)
         }
 
